ebf748dfb57d2e42a3f7a1ae04a7fa1b746c546d
Nick <nkinder1@hotmail.com>
2026-08-11 19:51:16 -0700

    codecrafters submit [skip ci]

---
+++ src/builtin.h
@@ -0,0 +1,6 @@
+//
+// Created by nkinder on 8/11/26.
+//
+
+#pragma once
+
--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 int repl();
 
@@ -8,19 +9,23 @@ int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  
+
   return repl();
 }
 
 int repl() {
     char command[1024];
-    while (1) {
+    bool running = true;
+    while (running) {
       printf("$ ");
       setbuf(stdout, NULL);
       const char* res = fgets(command, sizeof(command), stdin);
       if (res && !ferror(stdin)) { // Check to make sure some error hasn't occured before
         // trusting that command has a sane value
         command[strcspn(command, "\n")] = '\0'; // Remove the newline.
+        if (strncmp(command, "exit", 4) == 0) {
+          exit(EXIT_SUCCESS);
+        }
         printf("%s: command not found\n", command);
         setbuf(stdout, NULL);
       }

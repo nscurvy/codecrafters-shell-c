02e9dd4695a29bda915916baa46a03668bce81d4
Nick <nkinder1@hotmail.com>
2026-08-11 19:41:13 -0700

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
+  char command[1024];
   printf("$ ");
+  const char* res = fgets(command, sizeof(command), stdin);
+  if (res && !ferror(stdin)) { // Check to make sure some error hasn't occured before
+                               // trusting that command has a sane value
+    command[strcspn(command, "\n")] = '\0'; // Remove the newline.
+    printf("%s: command not found\n", command);
+  }
 
   return 0;
 }

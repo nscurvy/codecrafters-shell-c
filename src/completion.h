b539b448c3cbc7fb69801539f204ac07e5510f9b
Nick <nkinder1@hotmail.com>
2026-08-15 11:38:25 -0700

    codecrafters submit [skip ci]

--- src/completion.h
+++ src/completion.h
@@ -1,9 +1,18 @@
 //
 // Created by nkinder on 8/14/26.
 //
+//
+#include <glib-2.0/glib.h>
 
 
 #pragma once
+
+typedef struct CustomCompletion {
+    const char* name;
+
+};
+
+struct GHashTable* completion_table;
 
 // TODO: DOCS
 char* builtin_generator(const char* text, int state);
@@ -16,3 +25,4 @@ char* first_word_generator(const char* text, int state);
 
 // TODO: DOCS
 char** shell_completion_function(const char* text, int start, int end);
+
